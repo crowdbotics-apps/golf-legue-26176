6d7608327544b1d15d0d89319b9a95fc3485599e
Crowdbotics <team@crowdbotics.com>
2021-05-05 18:06:45 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/golf_legue_26176/MainActivity.java
@@ -0,0 +1,15 @@
+package com.golf_legue_26176;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "golf_legue_26176";
+  }
+}
---
+++ backend/golf_legue_26176/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for golf_legue_26176 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'golf_legue_26176.settings')
+
+application = get_wsgi_application()
